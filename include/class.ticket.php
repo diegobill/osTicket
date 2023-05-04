b2ae5c5bd3548c3bd9298eb49c0e017f6552cffb
diegobill <diegobill@gmail.com>
2023-05-04 08:11:59 -0300

    add getSourceEmail

--- include/class.ticket.php
+++ include/class.ticket.php
@@ -491,6 +491,10 @@ implements RestrictedAccess, Threadable, Searchable {
         return null;
     }
 
+    function getSourceEmail() {
+        return Email::lookup($this->email_id);
+    }
+
     function getReplyToEmail() {
         //TODO: Determine the email to use (once we enable multi-email support)
         return $this->getEmail();
